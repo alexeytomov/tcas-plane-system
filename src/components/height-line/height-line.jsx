78d3038bf08bebc91505e4e869ebdc10e42215c2
Alexey Tomov <tomovalexey@gmail.com>
2023-03-07 15:33:37 +0300

    feat: add plane models

--- src/components/height-line/height-line.jsx
+++ src/components/height-line/height-line.jsx
@@ -1,8 +1,12 @@
 import "./height-line.css";
 
+
+
 const HeightLine = (props) => {
     return (
-        <div className="line" style={props.style} />
+        <div className="line" style={props.style}>
+            <img className="plane" src={props.pic} alt=""></img>
+        </div>
     );
 }
 
